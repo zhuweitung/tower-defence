5a7c3eb4bca7f8363f918bfc84b7108cbf8dd93b
FrankCooper <marioba1997@163.com>
2017-08-02 20:14:11 +0800

    add viewController

---
+++ Assets/Scipts/ViewController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewController : MonoBehaviour {
+
+    public float speed = 60;
+	// Update is called once per frame
+    public float mouseSpeed = 500;
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float mouse = Input.GetAxis("Mouse ScrollWheel");
+        transform.Translate(new Vector3(h*speed, mouse*mouseSpeed, v*speed) * Time.deltaTime ,Space.World);
+	}
+}
