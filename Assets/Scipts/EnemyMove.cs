2e76c5a97c1acc06ac6ea7b3f37bdf1224303bdf
FrankCooper <marioba1997@163.com>
2017-08-03 11:16:26 +0800

    create enemyMove class

---
+++ Assets/Scipts/EnemyMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+    public float speed = 10;
+    private Transform[] positions;//拐点集合
+    private int index = 0;
+	// Use this for initialization
+	void Start () {
+        positions = WayPoints.positions;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        if (index > positions.Length - 1) return;//到达终点
+        transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * speed);
+        if (Vector3.Distance(positions[index].position, transform.position)<0.1f)
+        {
+            index++;
+        }
+    }
+}
