fcedac76388d4ffaa634bc3c02fd0924d5a25cd0
FrankCooper <marioba1997@163.com>
2017-08-03 12:27:45 +0800

    add wave class

---
+++ Assets/Scipts/Wave.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+//保存每一波敌人生成所需要的属性
+public class Wave {
+    public GameObject enemyPrefab;
+    public int count;//数量
+    public float rate;//间隔时间
+}
