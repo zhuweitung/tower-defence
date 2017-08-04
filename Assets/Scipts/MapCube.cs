29adedd5ce588bb153a3e633259a4d7a525167b8
FrankCooper <marioba1997@163.com>
2017-08-04 17:13:26 +0800

    create mapcube class

---
+++ Assets/Scipts/MapCube.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCube : MonoBehaviour {
+    [HideInInspector]
+    public GameObject turretGo;//保存mapcube上的炮台
+    public void BuildTurret(GameObject turretPrefab)
+    {
+
+    }
+}
