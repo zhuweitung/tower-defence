55300758a5f14b7a2a838fbf371e1c189673eded
FrankCooper <marioba1997@163.com>
2017-08-04 17:14:08 +0800

    modify buildmanager class
    add changemmoney function etc.

--- Assets/Scipts/BuildManager.cs
+++ Assets/Scipts/BuildManager.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BuildManager : MonoBehaviour {
     public TurretData laserTurretData;
     public TurretData missileTurretData;
     public TurretData standardTurretData;
     //当前选择的炮塔
-    public TurretData selectedTurretData;
+    private TurretData selectedTurretData;
+    public Text moneyText;
+    private int money = 1000;//初始金钱
+
+    void ChangeMoney(int change)
+    {
+        money += change;
+        moneyText.text = "￥" + money;
+    }
 
     void Update()
     {
         //当点击鼠标左键时
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             //当点击的不是ui时
             if (EventSystem.current.IsPointerOverGameObject() == false)
@@ -26,8 +35,28 @@ public class BuildManager : MonoBehaviour {
                 if (isCollider)
                 {
                     //得到点击的mapCube
-                    GameObject mapCube = hit.collider.gameObject;
-                    Debug.Log(mapCube.name);
+                    //GameObject mapCube = hit.collider.gameObject;
+                    MapCube mapcube = hit.collider.GetComponent<MapCube>();
+                    Debug.Log(mapcube.name);
+                    //mapcube上没有炮台
+                    if (mapcube.turretGo == null)
+                    {
+                        //金钱足够建造
+                        if (money > selectedTurretData.cost)
+                        {
+                            ChangeMoney(-selectedTurretData.cost);
+                            mapcube.BuildTurret(selectedTurretData.turretPrefab);
+                        }
+                        else
+                        {
+                            //提示钱不够
+                            Debug.Log("钱不够~");
+                        }
+                    }
+                    else
+                    {
+                        //升级炮台
+                    }
                 }
             }
         }
