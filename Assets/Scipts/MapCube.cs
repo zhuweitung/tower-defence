5439cee625c99b4798af04f1589b40f0be5d2434
FrankCooper <marioba1997@163.com>
2017-08-05 19:45:16 +0800

    Instantiate buildeffect

--- Assets/Scipts/MapCube.cs
+++ Assets/Scipts/MapCube.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class MapCube : MonoBehaviour {
     [HideInInspector]
     public GameObject turretGo;//保存mapcube上的炮台
+    public GameObject buildEffect;//建造炮台时的粒子特效
     public void BuildTurret(GameObject turretPrefab)
     {
         turretGo = GameObject.Instantiate(turretPrefab, transform.position, Quaternion.identity);
+        GameObject effect = GameObject.Instantiate(buildEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1);
     }
 }
