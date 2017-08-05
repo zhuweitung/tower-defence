4948a611c1a0b8a94ec48c262e558310658c5b72
FrankCooper <marioba1997@163.com>
2017-08-05 20:11:38 +0800

    create turret class

---
+++ Assets/Scipts/Turret.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    public List<GameObject> enemys = new List<GameObject>();//存储进入攻击范围的敌人
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Enemy")
+        {
+            enemys.Add(col.gameObject);
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Enemy")
+        {
+            enemys.Remove(col.gameObject);
+        }
+    }
+}
