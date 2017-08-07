a751b16943f529f4f275e2b429a38f07a6164eb9
FrankCooper <marioba1997@163.com>
2017-08-07 10:25:46 +0800

    add mapcube's mouse event

--- Assets/Scipts/MapCube.cs
+++ Assets/Scipts/MapCube.cs
@@ -1,15 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MapCube : MonoBehaviour {
     [HideInInspector]
     public GameObject turretGo;//保存mapcube上的炮台
     public GameObject buildEffect;//建造炮台时的粒子特效
+    private Renderer renderer;
+    private Color originalColor;
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        originalColor = renderer.material.color;
+    }
     public void BuildTurret(GameObject turretPrefab)
     {
         turretGo = GameObject.Instantiate(turretPrefab, transform.position, Quaternion.identity);
         GameObject effect = GameObject.Instantiate(buildEffect, transform.position, Quaternion.identity);
         Destroy(effect, 1);
+    }
+    void OnMouseEnter()
+    {
+        if (turretGo == null && EventSystem.current.IsPointerOverGameObject() == false)
+        {
+            renderer.material.color = Color.red;
+        }
+    }
+    void OnMouseExit()
+    {
+        renderer.material.color = originalColor;
     }
 }
