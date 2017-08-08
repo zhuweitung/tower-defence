9a9b147aa71debef632caba50d8ac8637724c183
FrankCooper <marioba1997@163.com>
2017-08-08 18:31:25 +0800

    add gamemenu class

---
+++ Assets/Scipts/GameMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+    public void OnButtonStartDown()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnButtonExitDown()
+    {
+        //在unity编辑器下退出游戏
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else        
+        Application.Quit();
+#endif
+
+    }
+}
